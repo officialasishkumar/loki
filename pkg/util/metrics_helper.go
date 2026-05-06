42c0753b59a10fb375fa14c9af66ac10272fd30f
Sandeep Sukhani <sandeep.d.sukhani@gmail.com>
2026-05-06 18:34:19 +0530

    update prometheus version

--- pkg/util/metrics_helper.go
+++ pkg/util/metrics_helper.go
@@ -10,12 +10,10 @@ import (
 
 	humanize "github.com/dustin/go-humanize"
 	"github.com/go-kit/log/level"
+	util_log "github.com/grafana/loki/v3/pkg/util/log"
 	"github.com/prometheus/client_golang/prometheus"
 	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/prometheus/model/labels"
-	tsdb_errors "github.com/prometheus/prometheus/tsdb/errors"
-
-	util_log "github.com/grafana/loki/v3/pkg/util/log"
 )
 
 var (
@@ -771,7 +769,7 @@ func GetLabels(c prometheus.Collector, filter map[string]string) ([]labels.Label
 		c.Collect(ch)
 	}()
 
-	errs := tsdb_errors.NewMulti()
+	var errs []error
 	var result []labels.Labels
 	dtoMetric := &dto.Metric{}
 	lbls := labels.NewBuilder(labels.EmptyLabels())
@@ -780,7 +778,7 @@ nextMetric:
 	for m := range ch {
 		err := m.Write(dtoMetric)
 		if err != nil {
-			errs.Add(err)
+			errs = append(errs, err)
 			// We cannot return here, to avoid blocking goroutine calling c.Collect()
 			continue
 		}
@@ -800,7 +798,7 @@ nextMetric:
 		result = append(result, lbls.Labels())
 	}
 
-	return result, errs.Err()
+	return result, errors.Join(errs...)
 }
 
 // DeleteMatchingLabels removes metric with labels matching the filter.
